21a76bb6cc2c8178c0891783c83e70f5d6d2030d
McBieg <127113578+McBieg@users.noreply.github.com>
2023-03-31 17:44:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,168 @@
-var currentPlayer = "X";
-var gameOver = false;
-var cells = document.querySelectorAll("td");
+const cards = [
+        "A",
+        "A",
+        "B",
+        "B",
+        "C",
+        "C",
+        "D",
+        "D",
+        "E",
+        "E",
+        "F",
+        "F",
+        "G",
+        "G",
+        "H",
+        "H",
+      ];
 
-cells.forEach(function(cell) {
-  cell.addEventListener("click", function() {
-    if (!gameOver && cell.innerHTML === "") {
-      cell.innerHTML = currentPlayer;
-      checkForWinner();
-      togglePlayer();
-    }
-  });
-});
+      let player1Score = 0;
+      let player2Score = 0;
+      let currentPlayer = 1;
+      let flippedCards = [];
+      let matchedCards = [];
 
-function checkForWinner() {
-  var winningCombinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
-    [0, 4, 8], [2, 4, 6] // diagonal
-  ];
+      const gameBoard = document.querySelector("#game-board");
+      const scoreBoard = document.querySelector("#score-board");
+      const message = document.querySelector(".message");
 
-  for (var i = 0; i < winningCombinations.length; i++) {
-    var combo = winningCombinations[i];
-    if (cells[combo[0]].innerHTML !== "" &&
-        cells[combo[0]].innerHTML === cells[combo[1]].innerHTML &&
-        cells[combo[1]].innerHTML === cells[combo[2]].innerHTML) {
-      gameOver = true;
-      alert("Gracz " + currentPlayer + " wygrał!");
-      break;
+      function shuffleCards() {
+        for (let i = cards.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [cards[i], cards[j]] = [cards[j], cards[i]];
+        }
+      }
+
+      function createCardElement(cardValue) {
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
+        cardElement.dataset.value = cardValue;
+        return cardElement;
+      }
+
+      function handleCardClick(event) {
+        const clickedCard = event.target;
+
+        if (
+          clickedCard.classList.contains("matched") ||
+          clickedCard.classList.contains("flipped")
+        ) {
+          return;
+        }
+
+        flippedCards.push(clickedCard);
+        clickedCard.classList.add("flipped");
+
+        if (flippedCards.length === 2) {
+          const card1Value = flippedCards[0].dataset.value;
+          const card2Value = flippedCards[1].dataset.value;
+
+          if (card1Value === card2Value) {
+            flippedCards.forEach((card) => {
+              card.classList.add("matched");
+              matchedCards.push(card);
+            });
+
+            flippedCards = [];
+
+            if (currentPlayer === 1) {
+              player1Score++;
+            } else {
+              player2Score++;
+            }
+          } else {
+            message.textContent = "Wrong!";
+            setTimeout(() => {
+              flippedCards.forEach((card) => {
+                card.classList.remove("flipped");
+              });
+              flippedCards = [];
+              message.textContent = "";
+
+              if (currentPlayer === 1) {
+                currentPlayer = 2;
+              } else {
+                currentPlayer
+Wysłano
+currentPlayer = 1;
+          }
+        }, 1000);
+      }
+
+      updateScoreBoard();
+      checkForEndOfGame();
+    } else if (currentPlayer === 2) {
+      setTimeout(computerPlayerTurn, 1000);
     }
   }
-}
 
-function togglePlayer() {
-  currentPlayer = currentPlayer === "X" ? "O" : "X";
-}
+  function computerPlayerTurn() {
+    const unmatchedCards = Array.from(
+      gameBoard.querySelectorAll(".card:not(.matched)")
+    );
+    const randomIndex = Math.floor(Math.random() * unmatchedCards.length);
+    const cardToFlip = unmatchedCards[randomIndex];
+
+    cardToFlip.classList.add("flipped");
+
+    flippedCards.push(cardToFlip);
+
+    setTimeout(() => {
+      const cardValue = cardToFlip.dataset.value;
+      const matchingCard = flippedCards.find(
+        (card) => card.dataset.value === cardValue
+      );
+
+      if (matchingCard) {
+        cardToFlip.classList.add("matched");
+        matchingCard.classList.add("matched");
+        matchedCards.push(cardToFlip, matchingCard);
+
+        flippedCards = [];
+
+        player2Score++;
+      } else {
+        cardToFlip.classList.remove("flipped");
+
+        flippedCards = [];
+
+        currentPlayer = 1;
+      }
+
+      updateScoreBoard();
+      checkForEndOfGame();
+    }, 1000);
+  }
+
+  function updateScoreBoard() {
+    scoreBoard.textContent = Player 1: ${player1Score} | Player 2: ${player2Score};
+  }
+
+  function checkForEndOfGame() {
+    if (matchedCards.length === cards.length) {
+      if (player1Score > player2Score) {
+        message.textContent = "Player 1 wins!";
+      } else if (player2Score > player1Score) {
+        message.textContent = "Player 2 wins!";
+      } else {
+        message.textContent = "It's a tie!";
+      }
+    }
+  }
+
+  function initGame() {
+    shuffleCards();
+
+    for (let i = 0; i < cards.length; i++) {
+      const cardElement = createCardElement(cards[i]);
+      cardElement.addEventListener("click", handleCardClick);
+      gameBoard.appendChild(cardElement);
+    }
+
+    updateScoreBoard();
+  }
+
+  initGame();
 
     
